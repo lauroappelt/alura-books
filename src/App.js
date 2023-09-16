db82947ddf7490bf85d760c71887d2ecb2424bc3
lauro <lauro.appelt@universo.univates.br>
2023-09-16 11:56:24 -0300

    Estilizando header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Logo from './componentes/Logo'
+import perfil from './imagens/perfil.svg';
+import sacola from './imagens/sacola.svg';
+
+const textoOpcoes = ['Categorias', 'Favoritos', 'Minha estante'];
+const icones = [perfil, sacola];
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='opcoes'>
+          {textoOpcoes.map((texto) => (
+            <li className='opcao'>{texto}</li>
+          ))}
+        </ul>
+        <ul className='icones'>
+          {icones.map((icone) => (
+            <li className='icone'><img src={icone}></img></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
